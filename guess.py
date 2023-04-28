8b5b8167e8a5d3d6108194ba96e7cd3607387b87
radha2323 <114419580+radha2323@users.noreply.github.com>
2023-04-28 12:58:29 +0530

    Create guess.py

---
+++ guess.py
@@ -0,0 +1,17 @@
+//importing random module
+import random
+import time       //time module
+print("welcome to the number guessing game:")
+time.sleep(5)   // for delaying
+guess=int(input("guess a number:"))
+while(n!=guess):
+  if(n>guess):
+    print("too low")
+    guess=int(input("guess again")
+  elif(n<guess):
+    print("too high")
+    guess=int(input("guess again")
+  else:
+     break
+print("***you nailed it!***")
+    
